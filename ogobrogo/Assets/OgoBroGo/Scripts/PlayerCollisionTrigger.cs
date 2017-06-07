97a415e193b048977decf7e1ba868ea4cd789f19
Lavender, Dan <Dan.Lavender@disney.com>
2017-06-06 19:28:43 -0700

    Added player and apple collectable prefab

---
+++ ogobrogo/Assets/OgoBroGo/Scripts/PlayerCollisionTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCollisionTrigger : MonoBehaviour 
+{
+	void OnTriggerEnter2D(Collider2D other)
+	{
+
+		if(other.gameObject.CompareTag("Collectable"))
+		{
+			Destroy(other.gameObject);
+		}
+
+	}
+
+}
