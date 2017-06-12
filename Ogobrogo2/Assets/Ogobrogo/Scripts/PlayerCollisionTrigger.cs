9854b2d17991933b70ae55ff3b93bc9a282916f8
Lavender, Dan <Dan.Lavender@disney.com>
2017-06-11 21:16:03 -0700

    Added some level objects, platforms and apple collection

---
+++ Ogobrogo2/Assets/Ogobrogo/Scripts/PlayerCollisionTrigger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerCollisionTrigger : MonoBehaviour 
+{
+	public Text AppleCount;
+	private int appleCount = 0;
+
+	void Start()
+	{
+		AppleCount.text = appleCount.ToString();
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+
+		if(other.gameObject.CompareTag("Collectable"))
+		{
+			Destroy(other.gameObject);
+			appleCount++;
+
+			AppleCount.text = appleCount.ToString();
+		}
+
+	}
+
+}
