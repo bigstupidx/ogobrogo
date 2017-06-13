cb141d69e3badc02e9b5822ad26b65a3efea5302
Lavender, Dan <Dan.Lavender@disney.com>
2017-06-12 21:55:31 -0700

    Added full length level and power animation (although power up timer isn’t working)

---
+++ Ogobrogo2/Assets/PowerUpController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Timers;
+
+public class PowerUpController : MonoBehaviour 
+{
+	public AudioSource audioSource;
+	public Animator animator;
+	public Rigidbody2D rigidBody;
+	public double PowerUpMaxTime = 2000;
+	private float startingGravity; 
+	private Timer powerUpTimer;
+
+	private Coroutine coroutine;
+
+	void Start()
+	{
+		startingGravity = rigidBody.gravityScale;
+
+		powerUpTimer = new Timer(); 
+		powerUpTimer.Elapsed += new ElapsedEventHandler(onTimer); 
+		powerUpTimer.Interval = PowerUpMaxTime; 
+	}
+
+
+	void onTimer(object source, ElapsedEventArgs e) 
+	{ 
+		animator.SetBool("PowerUp", false);
+		rigidBody.gravityScale = startingGravity;
+		powerUpTimer.Stop(); 
+	}
+
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+
+		if(other.gameObject.CompareTag("PowerUp"))
+		{
+			Destroy(other.gameObject);
+
+			animator.SetBool("PowerUp", true);
+
+			rigidBody.gravityScale = 3.5f;
+
+			audioSource.Play();
+
+			powerUpTimer.Start(); 
+		}
+	}
+
+}
